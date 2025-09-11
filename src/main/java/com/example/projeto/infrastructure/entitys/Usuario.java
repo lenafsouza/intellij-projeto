447713bed326baf22bd7c06f1fc481ef4dd2f9d3
lenafsouza <helena.francisco@unesp.br>
2025-09-11 20:18:35 -0300

    commit 11/09

---
+++ src/main/java/com/example/projeto/infrastructure/entitys/Usuario.java
@@ -0,0 +1,27 @@
+package com.example.projeto.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique = true, length = 200)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+
+
+
+}
